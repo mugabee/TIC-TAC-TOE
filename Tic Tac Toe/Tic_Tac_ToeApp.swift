6b54a2c7b9d6ff556b4e37931cb7e4f25d669081
mugabe richard <mugabe.richard.37@gmail.com>
2021-07-14 14:50:36 +0200

    getting started with the Tic Tac Toe Project

---
+++ Tic Tac Toe/Tic_Tac_ToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tic_Tac_ToeApp.swift
+//  Tic Tac Toe
+//
+//  Created by MUGABE RICHARD on 14/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct Tic_Tac_ToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
