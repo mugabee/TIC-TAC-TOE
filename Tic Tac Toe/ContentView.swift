67cf3c63b6fdbe6e8128f259d98ba6f8e56dc24f
mugabe richard <mugabe.richard.37@gmail.com>
2021-07-17 22:55:58 +0200

    My AI is Now SUPER amazing

--- Tic Tac Toe/ContentView.swift
+++ Tic Tac Toe/ContentView.swift
@@ -110,31 +110,38 @@ struct ContentView: View {
         let winPatterns: Set<Set<Int>> = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
         let computerMoves = moves.compactMap { $0 }.filter { $0.player ==  .computer}
         let computerPositions = Set(computerMoves.map { $0.boardIndex })
-        
+
         for pattern in winPatterns {
             let winPositions = pattern.subtracting(computerPositions)
-            
+
             if winPositions.count == 1 {
                 let isAvaiable = !isSquareOccupied(in: moves, forIndex: winPositions.first!)
                 if isAvaiable { return winPositions.first!}
             }
         }
-        // if AI can't win, then block
+       //  if AI can't win, then block
         let humanMoves = moves.compactMap { $0 }.filter { $0.player ==  .human }
         let humanPositions = Set(humanMoves.map { $0.boardIndex})
-        
+
         for pattern in winPatterns {
             let winPositions = pattern.subtracting(humanPositions)
-            
+
             if winPositions.count == 1 {
                 let isAvaiable = !isSquareOccupied(in: moves, forIndex: winPositions.first!)
                 if isAvaiable { return winPositions.first!}
             }
         }
         // if AI can't block, then take middle square
+        let centerSquare = 4
+        if !isSquareOccupied(in: moves, forIndex: centerSquare) {
+            return centerSquare
+    }
+        
         
         
         // if AI can't take middle square, take random Avilable square
+        
+        
         
         while isSquareOccupied(in: moves, forIndex: movePosition) {
            movePosition = Int.random(in: 0..<9)
